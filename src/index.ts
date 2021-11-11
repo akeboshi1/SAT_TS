2aa29ff9fedef11a0fef78e113f361abb9597b0b
guanxiaomin <327547931@qq.com>
2021-11-11 17:34:08 +0800

    update project webpack

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
-import {Box, Vector, Circle, Polygon}from './math';
+import { Box, Vector, Circle, Polygon } from './math';
 import Response from './collision/response';
-import {testCircleCircle,testPolygonCircle, testPolygonPolygon,testCirclePolygon} from './collision/util';
+import { testCircleCircle, testPolygonCircle, testPolygonPolygon, testCirclePolygon } from './collision/util';
 
-export {Box};
-export {Vector};
-export {Circle};
-export {Polygon};
-export {Response};
-export {testCircleCircle};
-export {testPolygonCircle};
-export {testPolygonPolygon};
-export {testCirclePolygon};
+export { Box };
+export { Vector };
+export { Circle };
+export { Polygon };
+export { Response };
+export { testCircleCircle };
+export { testPolygonCircle };
+export { testPolygonPolygon };
+export { testCirclePolygon };
